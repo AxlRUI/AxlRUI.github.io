a3b027754a8f18d66e1a457241ab79baf219d234
AxlRUI <axelpforestrubio@hotmail.com>
2016-04-17 13:53:17 -0500

    medic

--- proyectofinal/js/scripts.js
+++ proyectofinal/js/scripts.js
@@ -2,71 +2,13 @@ function registro(){
 
 	document.getElementById("change").className = "wrap";
 	document.getElementById("change2").className = "none";
+	document.getElementById("bienvenida").className = "none";
 
 };
 function login(){
 
 	document.getElementById("change2").className = "wrap";
 	document.getElementById("change").className = "none";
+	document.getElementById("bienvenida").className = "none";
 
 };
-(function(){
-
-var formulario = document.formulario_login,
-	elementos = formulario.elements;
-
-
-var validarInputs = function(){
-	for (var i = 0; i < elementos.length; i++) {
-
-		if (elementos[i].type == "text" || elementos[i].type == "password" || elementos[i].type== "email") {
-
-			if (elementos[i].value.length == 0) {
-				console.log('El campo ' + elementos[i].name + ' esta incompleto');
-				elementos[i].className = elementos[i].className + " error";
-				return false;
-			} else {
-				elementos[i].className = elementos[i].className.replace(" error", "");
-			}
-		}
-	}
-
-
-	return true;
-};
-
-
-
-var enviar = function(e){
-	if (!validarInputs()) {
-		console.log('Falto validar los Input');
-		e.preventDefault();
-	} else {
-		console.log('Envia');
-		e.preventDefault();
-	}
-};
-
-var focusInput = function(){
-	this.parentElement.children[1].className = "label active";
-	this.parentElement.children[0].className = this.parentElement.children[0].className.replace("error", "");
-};
-
-var blurInput = function(){
-	if (this.value <= 0) {
-		this.parentElement.children[1].className = "label";
-		this.parentElement.children[0].className = this.parentElement.children[0].className + " error";
-	}
-};
-
-
-formulario.addEventListener("submit", enviar);
-
-for (var i = 0; i < elementos.length; i++) {
-	if (elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password") {
-		elementos[i].addEventListener("focus", focusInput);
-		elementos[i].addEventListener("blur", blurInput);
-	}
-}
-
-}())
