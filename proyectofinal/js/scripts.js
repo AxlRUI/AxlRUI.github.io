41ad7ee5d485f9e015c565fe32d210a58ca68998
AxlRUI <axelpforestrubio@hotmail.com>
2016-05-28 17:03:23 -0500

    new
    new project

---
+++ proyectofinal/js/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(main);
+
+	var ok = 1;
+
+function main(){
+		$('.menu').click(function(){
+			if (ok == 1){
+				$('nav').animate({
+					left: '0'
+				});
+				ok = 0;
+			}else{
+				ok = 1;
+				$('nav').animate({
+					left: '-100%'
+				});
+			}
+		});
+		$('.submenu').click(function(){
+			$(this).children('.hijo').slideToggle();
+		});
+}
+function registro(){
+
+	document.getElementById("change").className = "wrap";
+	document.getElementById("change2").className = "none";
+	document.getElementById("bienvenida").className = "none";
+
+};
+function login(){
+
+	document.getElementById("change2").className = "wrap";
+	document.getElementById("change").className = "none";
+	document.getElementById("bienvenida").className = "none";
+
+};
+function valusuario(){
+
+	var input = document.getElementById("po"),
+		password = document.getElementById("pu");
+
+	if ((input.value == "user") && (password.value == "ok")) {
+
+		principal();
+
+	}else{
+		alert("Usuario o contraseña incorrectos");
+	}
+	return false;
+};
+function principal(){
+
+	document.getElementById("change2").className = "none";
+	document.getElementById("inicio").className = "inicio";
+	document.getElementById("header").className = "";
+	document.getElementById("reg").className = "none";
+
+};
